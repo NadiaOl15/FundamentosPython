0fc37b6ef633d408ca872d41eb4962130035e76f
sanchezyedra93@gmail.com <2471746880wicho@gmail.com>
2025-05-08 10:50:08 -0500

    Mi primer commit

---
+++ hello.py
@@ -0,0 +1,25 @@
+print("   ")
+print("Hello World!")
+print('Otro hello')
+
+name = 'Nadia'
+print("Hola " + name)
+print("  ")
+print("----------------------------------")
+print("  ")
+name2 = input("¿Quien eres?: ")
+print("Hola " + name2 + ", eres un menso baboso.")
+edad = input("¿Que edad tienes?: ")
+print("¿" + edad + " años sin bañarte?")
+
+
+
+
+
+
+
+
+
+
+
+
