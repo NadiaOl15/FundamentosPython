3f8ec49fa4bc9f8bb09ffef9417ed80731e2709b
sanchezyedra93@gmail.com <2471746880wicho@gmail.com>
2025-05-13 16:51:12 -0500

    ejercicio 1

--- ejercicio01.py
+++ ejercicio01.py
@@ -1,14 +1,2 @@
 name = input ("Escribe un nombre: \n")
 print("Hola, " + name + ".")
-
-print("Programa que calcula area y perimetro \n")
-
-altura = input("Ingresa la altura: \n")
-altura = int(altura)
-base = int (input("Ingresa la base: \n"))
-
-perimetro = base + altura + base + altura
-area = base * altura
-
-print ("El area del rectangulo es: " + str(area))
-print ("El perimetro del rectangulo es: " + str(perimetro))
