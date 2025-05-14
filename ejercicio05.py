7ad4e268298b99b1d2cb47f348edc9e31beda4c9
sanchezyedra93@gmail.com <2471746880wicho@gmail.com>
2025-05-14 14:34:16 -0500

    Ejercicio 5

---
+++ ejercicio05.py
@@ -0,0 +1,8 @@
+fahrenheit = float(input("Escribe grados Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5 / 9
+
+# 100ºF = 36ªC
+# Format strings
+# f ´{}'
+
+print(f'{fahrenheit}ºF equivalen a {celsius}ºC')
