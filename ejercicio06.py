1f63b08bbd51a0f8cd0dbf0d3743089ee9470106
sanchezyedra93@gmail.com <2471746880wicho@gmail.com>
2025-05-14 14:54:45 -0500

    Ejercicio 6

---
+++ ejercicio06.py
@@ -0,0 +1,11 @@
+print("Programa que promedia 3 nùmeros")
+print ("")
+
+numero1 = int(input("Ingresa el primer numero: "))
+numero2 = int(input("Ingresa el segundo numero: "))
+numero3 = int(input("Ingresa el tercer numero: "))
+
+promedio = (numero1 + numero2 + numero3) / 3
+
+print("El promedio de los nùmeros es: ", promedio)
+print("El promedio redondeado es: ", round(promedio))
