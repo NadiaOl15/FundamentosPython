f20569ab08e5f05573cc315ad14ad23d4878470e
sanchezyedra93@gmail.com <2471746880wicho@gmail.com>
2025-05-22 11:08:31 -0500

    Ejercicio1DeCondicionales

---
+++ condicionales/Ejercicio01.py
@@ -0,0 +1,12 @@
+################################################################################
+#Algoritmo que pida dos números e indique si el primero es mayor que el segundo.
+################################################################################
+
+numero1 = int(input("Ingresa un numero: "))
+numero2 = int(input("Ingresa otro numero: "))
+
+if numero1 > numero2:
+    print("El primer numero es mayor")
+
+else:
+    print("El primer numero es menor")
--- secuenciales/ejercicio06.py
+++ secuenciales/ejercicio06.py
@@ -8,4 +8,4 @@ numero3 = int(input("Ingresa el tercer numero: "))
 promedio = (numero1 + numero2 + numero3) / 3
 
 print("El promedio de los nùmeros es: ", promedio)
-print("El promedio redondeado es: ", round(promedio))
+print("El promedio redondeado de los nùmeros es: ", round(promedio))
