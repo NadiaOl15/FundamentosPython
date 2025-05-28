2ea75d67d01e396f028d34d30f4e1529adae9098
sanchezyedra93@gmail.com <2471746880wicho@gmail.com>
2025-05-28 14:57:23 -0500

    Ejercicio1For_Ciclos

--- ciclos/Ejercicio1For.py
+++ ciclos/Ejercicio1For.py
@@ -2,7 +2,7 @@
 #un número es el producto de todos los enteros entre 1 y el propio número y se 
 #representa por el número seguido de un signo de exclamación. 
 #Por ejemplo 5! = 1x2x3x4x5=120)
-##############################################################################
+###########################################################################
 
 numero = int(input('Ingresa un numero para calcular su factorial: '))
 factorial = 1
