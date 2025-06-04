2b83b5ccfcead766cb65f0bb774f37145ceac62d
NadiaOl15 <nadia.noya.ol@gmail.com>
2025-06-04 13:33:00 -0600

    Ejercicio04deStrings

--- Strings/Ejercicio04.py
+++ Strings/Ejercicio04.py
@@ -2,7 +2,7 @@
 # Suponiendo que hemos introducido una cadena por teclado que representa una frase 
 # (palabras separadas por espacios), realiza un programa que cuente cuantas 
 # palabras tiene.
-################################################################################
+###############################################################################
 
 phrase = input('Escribe una frase: ')
 
