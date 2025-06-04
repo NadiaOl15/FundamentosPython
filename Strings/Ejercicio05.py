2204d2adf7a02ae831ae7f72b95d75b8531b2e1f
NadiaOl15 <nadia.noya.ol@gmail.com>
2025-06-04 13:33:28 -0600

    Ejercicio05deStrings

--- Strings/Ejercicio05.py
+++ Strings/Ejercicio05.py
@@ -1,7 +1,7 @@
 ################################################################################
 # Si tenemos una cadena con un nombre y apellidos, realizar un programa que 
 # muestre las iniciales en mayúsculas.
-###############################################################################
+##############################################################################
 
 full_name = input('Escribe tu nombre completo: ')
 
